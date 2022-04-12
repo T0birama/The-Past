f74cb113402c57017059161c78c34a8a7bfa385e
MartinToledo <66489967+T0birama@users.noreply.github.com>
2022-04-12 19:38:16 -0400

    TESTING

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,15 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
         
     public float speed = 12f;
+    public float gravity = -9.81f;
+
+    Vector3 velocity;
+    
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +26,13 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -24,5 +40,7 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
