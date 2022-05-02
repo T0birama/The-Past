c79b3803289ecbf0a91aa737cd4b308f648aba21
T0birama <ofertade99@gmail.com>
2022-05-02 18:16:41 -0400

    aaa

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -5,57 +5,22 @@ using UnityEngine.Rendering;
 
 public class Inventory : MonoBehaviour
 {
-    private bool inventoryEnabled;
+    public GameObject Canvas;
     
-    public GameObject inventory;
-
-    public int allSlots;
-
-    private int enabledSlot;
-
-    public GameObject[] slot;
-
-    public GameObject slotHandler;
-    public MouseLook mouseLook;
     private void Start()
     {
         
-        allSlots = slotHandler.transform.childCount;
-
-        slot = new GameObject[allSlots];
-
-        for (int i = 0; i < allSlots; i++)
-        {
-            slot[i] = slotHandler.transform.GetChild(i).gameObject;
-
-            if(slot[i].GetComponent<Slot>().item == null)
-            {
-                slot[i].GetComponent<Slot>().empty = true;
-            }
-        }
+        
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            inventoryEnabled=!inventoryEnabled;
+            
         }
 
-        if (inventoryEnabled == true)
-        {
-            inventory.SetActive(true);
-            mouseLook.mouseSensivility = 0;
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-        }
-        else
-        {
-            inventory.SetActive(false);
-            mouseLook.mouseSensivility = 200;
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-        }
+       
     }
 
 
