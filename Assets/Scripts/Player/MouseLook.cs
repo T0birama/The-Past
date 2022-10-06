5dca9514279f65044afb5c13595cb9bcebe94502
MartinToledo <ofertade99@gmail.com>
2022-10-06 15:42:46 -0300

    wow

--- Assets/Scripts/Player/MouseLook.cs
+++ Assets/Scripts/Player/MouseLook.cs
@@ -7,14 +7,16 @@ public class MouseLook : MonoBehaviour
 {
     public GameObject mano;
     [Header ("Mouse Settings")]
-    public float distance = 15;
-    public float distance2 = 100;
+    public float distance = 1f;
+    
     public float mouseSensivility = 200f;
 
     public Transform playerBody;
     [Header ("Objetos/linterna/Medicamento")]
     public ObjON linternaObj;
     public Item medicamentoObj;
+
+    public RaycastHit hit;
 
     [Header("Puertas")]
 
@@ -32,36 +34,21 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        RaycastHit[] hits = Physics.RaycastAll(transform.position , transform.forward, distance);
-        RaycastHit[] hits2 = Physics.RaycastAll(transform.position , transform.forward, distance2);
+       
         Debug.DrawRay(transform.position, transform.forward * distance, Color.red);
 
-        foreach (RaycastHit hit2 in hits2)
+        if (Physics.Raycast(transform.position, transform.forward, out hit, distance))
         {
-            Debug.Log(hit2.collider.tag);
-
-            if (hit2.collider.CompareTag("Untagged"))
-            {
-                mano.SetActive(false);
-                linternaObj.TextLinternaPuedo.SetActive(false);
-                linternaObj.isOnlinte = false;
-                medicamentoObj.textoRecogerMedi.SetActive(false);
-                medicamentoObj.IsOnMedi = false;
-
-            }
-            else if (hit2.collider.CompareTag("Fantasmita"))
-            {
-                hit2.collider.gameObject.SetActive(false);
-            }
-        }
-        foreach (RaycastHit hit in hits)
-        {
-            Debug.Log(hit.collider.tag);
+            
             
             if (hit.collider.CompareTag("medicamento"))
             {
-                medicamentoObj.IsOnMedi = true; 
+
+                //hit.collider.gameObject.GetComponent<Item>().IsOnMedi = true;
+                //hit.collider.gameObject.GetComponent<Item>().textoRecogerMedi.SetActive(true);
+                medicamentoObj.IsOnMedi = true;
                 medicamentoObj.textoRecogerMedi.SetActive(true);
+
             }
             else if (hit.collider.CompareTag("linterna"))
             {
@@ -73,6 +60,19 @@ public class MouseLook : MonoBehaviour
                 hit.collider.gameObject.SetActive(false);
                 timelineCosa.timelineplay();
             }
+            else if (hit.collider.CompareTag("Untagged"))
+            {
+                mano.SetActive(false);
+                linternaObj.TextLinternaPuedo.SetActive(false);
+                linternaObj.isOnlinte = false;
+                medicamentoObj.textoRecogerMedi.SetActive(false);
+                medicamentoObj.IsOnMedi = false;
+
+            }
+            else if (hit.collider.CompareTag("Fantasmita"))
+            {
+                hit.collider.gameObject.SetActive(false);
+            }
             else
             {
                 medicamentoObj.textoRecogerMedi.SetActive(false);
@@ -80,13 +80,19 @@ public class MouseLook : MonoBehaviour
                 linternaObj.isOnlinte = false;
                 mano.SetActive(false);
                 linternaObj.TextLinternaPuedo.SetActive(false);
-
             }
-
+        }
+        else
+        {
+            medicamentoObj.textoRecogerMedi.SetActive(false);
+            medicamentoObj.IsOnMedi = false;
+            linternaObj.isOnlinte = false;
+            mano.SetActive(false);
+            linternaObj.TextLinternaPuedo.SetActive(false);
         }
 
 
-        
+
 
         float mouseX = Input.GetAxis("Mouse X") * mouseSensivility * Time.deltaTime; 
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensivility * Time.deltaTime;
