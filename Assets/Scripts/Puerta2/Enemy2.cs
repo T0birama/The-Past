078736370e5d1728837e95eeaa3f20c1c6ba7efe
MartinToledo <ofertade99@gmail.com>
2022-12-14 20:55:35 -0300

    a

--- Assets/Scripts/Puerta2/Enemy2.cs
+++ Assets/Scripts/Puerta2/Enemy2.cs
@@ -9,6 +9,8 @@ public class Enemy2 : MonoBehaviour
     public float TiempoDeSpeed = 5;
     public Transform player;
     public Animator Animator1;
+    public AudioSource a1;
+   
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +21,7 @@ public class Enemy2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Enemy.SetDestination(player.transform.position);
+        //Enemy.SetDestination(player.transform.position);
         
     }
 
@@ -27,17 +29,25 @@ public class Enemy2 : MonoBehaviour
     {
         Animator1.SetFloat("multi", 1f);
         Enemy.SetDestination(player.transform.position);
-        Enemy.enabled = true;
+        Enemy.acceleration = 100;
+        Enemy.angularSpeed = 100;
         Enemy.speed = 30;
-
+        a1.volume = 0.5f;
+        a1.pitch = 1.5f;
+       
     }
 
     public void StopPlayer()
     {
         Animator1.SetFloat("multi", 0f);
+        Enemy.SetDestination(transform.position);
         Enemy.speed = 0;
-        Enemy.enabled = false;
+        Enemy.acceleration = 0;
+        Enemy.angularSpeed = 0;
         StartCoroutine(VolverSpeed());
+        a1.volume = 1f;
+        a1.pitch = 0.6f;
+  
     }
 
     IEnumerator VolverSpeed()
